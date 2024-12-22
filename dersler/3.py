56d88618a1c31eb5b2dc39e63d82635896b1d761
odul-okunakol <odulokunakol@gmail.com>
2024-12-22 14:57:58 +0100

    3.py dosyasını ekledim veya güncelledim

---
+++ dersler/3.py
@@ -0,0 +1,6 @@
+sayilistes=[10,25,7,42,19]
+for i in sayilistes:
+    print('Listeniz:', sayilistes)
+    print('En kücük sayi:', min(sayilistes))
+    print('En büyük sayi:', max(sayilistes))
+    break
