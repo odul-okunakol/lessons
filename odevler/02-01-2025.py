b04b6f8378ad976a4e6a58d92693ae99e30887b7
Deniz Parlak <parlak.deniss@gmail.com>
2025-01-02 10:33:43 +0300

    2 Ocak ödevi eklendi

---
+++ odevler/02-01-2025.py
@@ -0,0 +1,71 @@
+""""
+Pozitif Sayıları Toplama
+Bir liste içerisindeki pozitif sayıları bulan ve bunların toplamını döndüren bir fonksiyon yazın.
+Eğer liste boşsa veya pozitif sayı yoksa 0 döndürsün.
+Beklenen Çıktı:
+
+Girdi: [-10, 15, -20, 30, -5]
+Çıktı: 45
+"""
+
+
+"""
+Benzersiz Elemanları Bulma
+Bir liste içerisindeki tekrar eden elemanları çıkararak sadece benzersiz elemanları döndüren bir fonksiyon yazın.
+Beklenen Çıktı:
+
+Girdi: [1, 2, 2, 3, 4, 4, 5]
+Çıktı: [1, 3, 5]
+"""
+
+"""
+ Sözlükte Değer Arama
+Bir sözlükte verilen bir anahtarın olup olmadığını kontrol eden bir fonksiyon yazın.
+Eğer anahtar mevcutsa, değerini döndürün. Mevcut değilse bir hata mesajı yazdırın.
+Beklenen Çıktı:
+
+Girdi: {"ad": "Ali", "yas": 25, "meslek": "Mühendis"}, Anahtar: "yas"
+Çıktı: 25
+"""
+
+"""
+Sayı Tahmin Oyunu
+Kullanıcıdan bir sayı girmesini isteyin. Sayı, bilgisayar tarafından rastgele belirlenen bir sayı ile karşılaştırılsın.
+Kullanıcı doğru tahmin edene kadar ipuçları (daha büyük ya da daha küçük) verin. Beklenen Çıktı:
+
+Bilgisayarın seçtiği sayı: 8
+Kullanıcı tahmini: 5
+Çıktı: Daha büyük!
+
+"""
+
+"""
+Karakter Frekansı
+Kullanıcıdan bir cümle alın ve her harfin kaç kez geçtiğini gösteren bir sözlük döndürün.
+Beklenen Çıktı:
+
+Girdi: "merhaba"
+Çıktı: {"m": 1, "e": 1, "r": 1, "h": 1, "a": 2, "b": 1}
+"""
+
+"""
+Şifre Doğrulama
+Kullanıcıdan bir şifre alın ve şu kurallara uygun olup olmadığını kontrol edin:
+En az 8 karakter uzunluğunda olmalı.
+En az bir büyük harf içermeli.
+En az bir sayı içermeli.
+Geçerli değilse hangi kuralın ihlal edildiğini yazdırın.
+Beklenen Çıktı:
+
+Girdi: "Python123"
+Çıktı: Şifre geçerli!
+"""
+
+"""
+En Uzun Kelime
+Kullanıcıdan bir cümle alın ve cümledeki en uzun kelimeyi döndüren bir fonksiyon yazın.
+Beklenen Çıktı:
+
+Girdi: "Python harika bir programlama dili"
+Çıktı: programlama
+"""
