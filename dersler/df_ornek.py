d85170f9a39bb209f8cc294d9b7c1bffb8076535
odul-okunakol <odulokunakol@gmail.com>
2025-01-12 16:12:26 +0100

    eklendi

---
+++ dersler/df_ornek.py
@@ -0,0 +1,11 @@
+data = {
+    "Departman": ["IT", "IT", "HR", "HR", "Finance", "Finance"],
+    "Çalışan": ["Ali", "Ayşe", "Mehmet", "Fatma", "Ahmet", "Zeynep"],
+    "Maaş": [4000, 4500, 3500, 3000, 6000, 6200]
+}
+
+import pandas as pd
+df = pd.DataFrame(data)
+
+def maas_ortalama(df):
+    df.groupby[]
